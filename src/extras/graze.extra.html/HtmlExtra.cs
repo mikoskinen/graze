49db1f4251a3ca1309b776152592c7bb19a077ba
Mikael Koskinen <mijapkos@gmail.com>
2012-02-21 17:20:43 +0200

    v3.0.0 release

---
+++ src/extras/graze.extra.html/HtmlExtra.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using System.Net;
+using System.Xml.Linq;
+using graze.contracts;
+
+namespace graze.extra.html
+{
+    [Export(typeof(IExtra))]
+    public class HtmlExtra : IExtra
+    {
+        public bool CanProcess(XElement element)
+        {
+            return element != null && element.Name.LocalName.Equals("Html");
+        }
+
+        public object GetExtra(XElement element)
+        {
+            var xAttribute = element.Attribute("Url");
+            if (xAttribute == null)
+                return null;
+
+            var client = new WebClient();
+            return client.DownloadString(xAttribute.Value);
+        }
+    }
+}
---
+++ src/extras/graze.extra.youtube/YoutubeExtra.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Xml.Linq;
+using graze.contracts;
+
+namespace graze.extra.youtube
+{
+    [Export(typeof(IExtra))]
+    public class YoutubeExtra : IExtra
+    {
+        public bool CanProcess(XElement element)
+        {
+            return element != null && element.Name.LocalName.Equals("Youtube");
+        }
+
+        public object GetExtra(XElement element)
+        {
+            var urlAttribute = element.Attribute("Url");
+            if (urlAttribute == null)
+                return null;
+
+            var width = 560;
+            var widthAttribute = element.Attribute("Width");
+            if (widthAttribute != null)
+                width = Convert.ToInt32(widthAttribute.Value);
+
+            var height = 315;
+            var heightAttribute = element.Attribute("Height");
+            if (heightAttribute != null)
+                height = Convert.ToInt32(heightAttribute.Value);
+
+            return string.Format("<iframe width=\"{0}\" height=\"{1}\" src=\"{2}\" frameborder=\"0\" allowfullscreen></iframe>", width, height, urlAttribute.Value) ;
+        }
+    }
+}
